2d873994465f65647c1e6d5b3d083005f623b50f
ankkko_neko <sasniypubger1@gmail.com>
2024-07-07 13:05:05 +0300

    Update cockloader.py

--- cockloader.py
+++ cockloader.py
@@ -1,24 +1,14 @@
+import imghdr
 import io
 import os
 import random
-import re
-
-import requests
-from telethon.errors.rpcerrorlist import YouBlockedUserError
-from telethon.tl.types import Message
-
-from .. import loader, utils
-
-
-@loader.tds
-class FileUploaderMod(loader.Module):
-    """Different engines file uploader"""
-
-    strings = {
-        "name": "Uploader",
+@@ -20,19 +19,14 @@ class FileUploaderMod(loader.Module):
         "uploading": "🚀 <b>Uploading...</b>",
         "noargs": "🚫 <b>No file specified</b>",
         "err": "🚫 <b>Upload error</b>",
+        "uploaded": '🎡 <b>File <a href="{0}">uploaded</a></b>!\n\n<code>{0}</code>',
+        "imgur_blocked": "🚫 <b>Unban @ImgUploadBot</b>",
+        "not_an_image": "🚫 <b>This platform only supports images</b>",
         "uploaded": '🎡 <b>File <a href="{0}">uploaded</a></b>!\n\n<code>{0}</code
     }
 
@@ -26,106 +16,73 @@ class FileUploaderMod(loader.Module):
         "uploading": "🚀 <b>Загрузка...</b>",
         "noargs": "🚫 <b>Файл не указан</b>",
         "err": "🚫 <b>Ошибка загрузки</b>",
+        "uploaded": '🎡 <b>Файл <a href="{0}">загружен</a></b>!\n\n<code>{0}</code>',
+        "imgur_blocked": "🚫 <b>Разблокируй @ImgUploadBot</b>",
+        "not_an_image": "🚫 <b>Эта платформа поддерживает только изображения</b>",
+        "_cmd_doc_imgur": "Загрузить на imgur.com",
         "uploaded": '🎡 <b>Файл <a href="{0}">загружен</a></b>!\n\n<code>{0}</code
         "_cmd_doc_oxo": "Загрузить на 0x0.st",
         "_cmd_doc_x0": "Загрузить на x0.at",
         "_cmd_doc_skynet": "Загрузить на децентрализованную платформу SkyNet",
-        "_cmd_doc_kappa": "Загрузить на kappa.lol",
-        "_cls_doc": "Загружает файлы на различные хостинги",
-    }
+@@ -71,17 +65,6 @@ async def get_media(self, message: Message):
 
-    async def get_media(self, message: Message):
-        reply = await message.get_reply_message()
-        m = None
-        if reply and reply.media:
-            m = reply
-        elif message.media:
-            m = message
-        elif not reply:
-            await utils.answer(message, self.strings("noargs"))
+        return file
+
+    async def get_image(self, message: Message):
+        file = await self.get_media(message)
+        if not file:
             return False
 
-        if not m:
-            file = io.BytesIO(bytes(reply.raw_text, "utf-8"))
-            file.name = "file.txt"
-        else:
-            file = io.BytesIO(await self._client.download_media(m, bytes))
-            file.name = (
-                m.file.name
-                or (
-                    "".join(
-                        [
-                            random.choice("abcdefghijklmnopqrstuvwxyz1234567890")
-                            for _ in range(16)
-                        ]
-                    )
-                )
-                + m.file.ext
-            )
+        if imghdr.what(file) not in ["gif", "png", "jpg", "jpeg", "tiff", "bmp"]:
+            await utils.answer(message, self.strings("not_an_image"))
+            return False
 
         return file
 
     async def skynetcmd(self, message: Message):
         """Upload to decentralized SkyNet"""
         message = await utils.answer(message, self.strings("uploading"))
-        file = await self.get_media(message)
-        if not file:
-            return
-
-        try:
-            skynet = await utils.run_sync(
-                requests.post,
-                "https://siasky.net/skynet/skyfile",
-                files={"file": file},
-            )
-        except ConnectionError:
-            await utils.answer(message, self.strings("err"))
-            return
-
-        await utils.answer(
-            message,
-            self.strings("uploaded").format(
-                f"https://siasky.net/{skynet.json()['skylink']}"
+@@ -106,42 +89,7 @@ async def skynetcmd(self, message: Message):
             ),
         )
 
+    async def imgurcmd(self, message: Message):
+        """Upload to imgur.com"""
+        message = await utils.answer(message, self.strings("uploading"))
+        file = await self.get_image(message)
+        if not file:
+            return
+
+        chat = "@ImgUploadBot"
+
+        async with self._client.conversation(chat) as conv:
+            try:
+                m = await conv.send_message(file=file)
+                response = await conv.get_response()
+            except YouBlockedUserError:
+                await utils.answer(message, self.strings("imgur_blocked"))
+                return
+
+            await m.delete()
+            await response.delete()
+
+            try:
+                url = (
+                    re.search(
+                        r'<meta property="og:image" data-react-helmet="true"'
+                        r' content="(.*?)"',
+                        (await utils.run_sync(requests.get, response.raw_text)).text,
+                    )
+                    .group(1)
+                    .split("?")[0]
+                )
+            except Exception:
+                url = response.raw_text
+
+            await utils.answer(message, self.strings("uploaded").format(url))
+
+    async def oxocmd(self, message: Message):
 async def oxocmd(self, message: Message):
         """Upload to 0x0.st"""
         message = await utils.answer(message, self.strings("uploading"))
         file = await self.get_media(message)
-        if not file:
-            return
-
-        try:
-            oxo = await utils.run_sync(
-                requests.post,
-                "https://0x0.st",
-                files={"file": file},
-                data={"secret": True},
-            )
-        except ConnectionError:
-            await utils.answer(message, self.strings("err"))
-            return
-
-        url = oxo.text
-        await utils.answer(message, self.strings("uploaded").format(url))
-
-    async def kappacmd(self, message: Message):
-        """Upload to kappa.lol"""
-        message = await utils.answer(message, self.strings("uploading"))
-        file = await self.get_media(message)
-        if not file:
-            return
-
-        try:
-            kappa = await utils.run_sync(
-                requests.post,
-                "https://kappa.lol/api/upload",
-                files={"file": file},
-            )
-        except ConnectionError:
-            await utils.answer(message, self.strings("err"))
-            return
-
-        url = f"https://kappa.lol/{kappa.json()['id']}"
-        await utils.answer(message, self.strings("uploaded").format(url))
