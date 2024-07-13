0a779bb3c2c9f17afb83a75d9489ec4aac939f98
ankkko_neko <sasniypubger1@gmail.com>
2024-07-13 19:51:22 +0300

    Update cockloader.py

--- cockloader.py
+++ cockloader.py
@@ -21,7 +21,6 @@ class FileUploaderMod(loader.Module):
         "noargs": "🚫 <b>No file specified</b>",
         "err": "🚫 <b>Upload error</b>",
         "uploaded": '🎡 <b>File <a href="{0}">uploaded</a></b>!\n\n<code>{0}</code>',
-        "imgur_blocked": "🚫 <b>Unban @ImgUploadBot</b>",
         "not_an_image": "🚫 <b>This platform only supports images</b>",
     }
 
@@ -30,12 +29,9 @@ class FileUploaderMod(loader.Module):
         "noargs": "🚫 <b>Файл не указан</b>",
         "err": "🚫 <b>Ошибка загрузки</b>",
         "uploaded": '🎡 <b>Файл <a href="{0}">загружен</a></b>!\n\n<code>{0}</code>',
-        "imgur_blocked": "🚫 <b>Разблокируй @ImgUploadBot</b>",
         "not_an_image": "🚫 <b>Эта платформа поддерживает только изображения</b>",
-        "_cmd_doc_imgur": "Загрузить на imgur.com",
         "_cmd_doc_oxo": "Загрузить на 0x0.st",
         "_cmd_doc_x0": "Загрузить на x0.at",
-        "_cmd_doc_skynet": "Загрузить на децентрализованную платформу SkyNet",
         "_cmd_doc_kappa": "Загрузить на kappa.lol",
         "_cls_doc": "Загружает файлы на различные хостинги",
     }
@@ -81,65 +77,6 @@ class FileUploaderMod(loader.Module):
             return False
 
         return file
-
-    async def skynetcmd(self, message: Message):
-        """Upload to decentralized SkyNet"""
-        message = await utils.answer(message, self.strings("uploading"))
-        file = await self.get_media(message)
-        if not file:
-            return
-
-        try:
-            skynet = await utils.run_sync(
-                requests.post,
-                "https://siasky.net/skynet/skyfile",
-                files={"file": file},
-            )
-        except ConnectionError:
-            await utils.answer(message, self.strings("err"))
-            return
-
-        await utils.answer(
-            message,
-            self.strings("uploaded").format(
-                f"https://siasky.net/{skynet.json()['skylink']}"
-            ),
-        )
-
-    async def imgurcmd(self, message: Message):
-        """Upload to imgur.com"""
-        message = await utils.answer(message, self.strings("uploading"))
-        file = await self.get_image(message)
-        if not file:
-            return
-
-        chat = "@ImgUploadBot"
-
-        async with self._client.conversation(chat) as conv:
-            try:
-                m = await conv.send_message(file=file)
-                response = await conv.get_response()
-            except YouBlockedUserError:
-                await utils.answer(message, self.strings("imgur_blocked"))
-                return
-
-            await m.delete()
-            await response.delete()
-
-            try:
-                url = (
-                    re.search(
-                        r'<meta property="og:image" data-react-helmet="true"'
-                        r' content="(.*?)"',
-                        (await utils.run_sync(requests.get, response.raw_text)).text,
-                    )
-                    .group(1)
-                    .split("?")[0]
-                )
-            except Exception:
-                url = response.raw_text
-
-            await utils.answer(message, self.strings("uploaded").format(url))
 
     async def oxocmd(self, message: Message):
         """Upload to 0x0.st"""
